90ecce74a9463413365dd2f2a39c33df4b5014b8
Pradnya <pradnyabhosale317@gmail.com>
2026-03-01 22:13:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from message_ai import get_ai_response
 user_question = st.text_input("Ask your medicine/symptom question:")
 
@@ -5,3 +6,4 @@ if st.button("Ask AI"):
     answer = get_ai_response(user_question)
 
     st.write(answer)
+
